e34f1ea5a275de591fa6f1eb1172112cb6429961
David Schaller <38377210+david-schaller@users.noreply.github.com>
2021-09-16 15:30:33 +0200

    loose consensus tree

--- src/tralda/consensus/LooseConsensus.py
+++ src/tralda/consensus/LooseConsensus.py
@@ -11,21 +11,139 @@ __author__ = 'David Schaller'
 from tralda.datastructures.Tree import Tree, TreeNode, LCA
 
 
-def merge_trees(T1, T2):
+def loose_consensus_tree(trees):
+    """Compute the loose consensus tree for trees with the same leaf set.
     
-    # list of leaves ordered as in T1
-    L = [l for l in T1.leaves()]
-    n = len(L)
+    All input trees must be phylogenetic and have the same set of leaf IDs.
+    In each tree, the leaf IDs must be unique.
+    
+    Parameters
+    ----------
+    trees : sequence of Tree instances
+    
+    Returns
+    -------
+    Tree
+        The loose consensus tree; None if 'trees' is empty.
+        
+    Raises
+    ------
+    TypeError
+        In case of input instances that are not of type Tree.
+    RuntimeError
+        If the sequence contains empty trees or the tree do not share the same
+        set of leaves.
+        
+    References
+    ----------
+    .. [1] J. Jansson, C. Shen, W.-K. Sung.
+       Improved Algorithms for Constructing Consensus Trees.
+       In: J. ACM 63, 3, Article 28 (June 2016), 24 pages.
+       doi:10.1145/2925985.
+    """
+    
+    LC = LooseConsensusTree(trees)
+    return LC.run()
+
+
+class LooseConsensusTree:
+    
+    def __init__(self, trees):
+        
+        _assert_leaf_sets_equal(trees)
+        self.trees = trees
+    
+    
+    def run(self):
+        
+        self.total_contracted = 0
+        
+        k = len(self.trees)
+        if k == 0:
+            return None
+        elif k == 1:
+            return self.trees[0].copy()
+        
+        T = self.trees[0]
+        for j in range(1, k):
+            A, x = one_way_compatible(T, self.trees[j],
+                                      return_no_of_contractions=True)
+            self.total_contracted += x
+            T = merge_trees(A, self.trees[j])
+        
+        for j in range(k):
+            T, x = one_way_compatible(T, self.trees[j],
+                                      return_no_of_contractions=True)
+            self.total_contracted += x
+        
+        self.T = T
+        return self.T
+
+
+def merge_all(trees):
+    """Common refinement for compatible trees with the same leaf set.
+    
+    All input trees must be phylogenetic and have the same set of leaf IDs.
+    In each tree, the leaf IDs must be unique.
+    
+    Warning: The behavior of the function is undefined if the trees are not
+    compatible, i.e., if no common refinement exists.
+    
+    Parameters
+    ----------
+    trees : sequence of Tree instances
+    
+    Returns
+    -------
+    Tree
+        A common refinement of the input trees; None if 'trees' is empty.
+    """
+    
+    _assert_leaf_sets_equal(trees)
+    
+    k = len(trees)
+    if k == 0:
+        return None
+    elif k == 1:
+        return trees[0].copy()
+    
+    T = trees[0]
+    for j in range(1, k):
+        T = merge_trees(T, trees[j])
+    
+    return T
+
+
+def _assert_leaf_sets_equal(trees):
+    
+    L = None
+        
+    for T_i in trees:
+        
+        if not isinstance(T_i, Tree):
+            raise TypeError("not a 'Tree' instance")
+            
+        if not L:
+            L = {v.ID for v in T_i.leaves()}
+            if len(L) == 0:
+                raise RuntimeError('empty tree in tree list')
+        else:
+            L2 = {v.ID for v in T_i.leaves()}
+            if L != L2:
+                raise RuntimeError('trees must have the same leaf IDs')
+
+
+def _preprocess(T1, T2):
     
     # bijection such that clusters in T1 form sequences os consecutive integers
-    f = {l.ID: i for i, l in enumerate(L)}
+    f = {l.ID: i for i, l in enumerate(T1.leaves())}
     
     # dict storing the minimal f(x) for each vertex in T2
     m = {}
     for v in T2.postorder():
         m[v] = min(m[c] for c in v.children) if v.children else f[v.ID]
     
-    counting_sort = [[] for i in range(n)]
+    counting_sort = [[] for i in range(len(f))]
     for v in T2.preorder():
         counting_sort[m[v]].append(v)
     sorted_vertices = [v for sublist in counting_sort for v in sublist]
@@ -77,6 +195,36 @@ def merge_trees(T1, T2):
     
     lca = LCA(T2)
     
+    return T2, L_T2, lca, depth, start, stop, x_left, x_right
+
+
+def merge_trees(T1, T2):
+    """Common refinement of two compatible trees with the same leaf set.
+    
+    Both input trees must be phylogenetic and have the same set of leaf IDs.
+    In each tree, the leaf IDs must be unique.
+    
+    Parameters
+    ----------
+    T1 : Tree
+    T2 : Tree
+    
+    Returns
+    -------
+    Tree
+        A tree whose clusters are the union of the cluster of T1 and T2.
+        
+    References
+    ----------
+    .. [1] J. Jansson, C. Shen, W.-K. Sung.
+       Improved Algorithms for Constructing Consensus Trees.
+       In: J. ACM 63, 3, Article 28 (June 2016), 24 pages.
+       doi:10.1145/2925985.
+    """
+    
+    # T2 gets copied in _preprocess, so input trees remain unchanged
+    T2, L_T2, lca, depth, start, stop, x_left, x_right = _preprocess(T1, T2)
+    
     for u in T1.postorder():
         
         if not u.children:
@@ -109,6 +257,70 @@ def merge_trees(T1, T2):
     
     return T2
 
+
+def one_way_compatible(T1, T2, return_no_of_contractions=False):
+    """Remove all clusters in a tree T1 that are incompatible with T2.
+    
+    Both input trees must be phylogenetic and have the same set of leaf IDs.
+    In each tree, the leaf IDs must be unique.
+    
+    Parameters
+    ----------
+    T1 : Tree
+    T2 : Tree
+    
+    Returns
+    -------
+    Tree
+        A copy of T1 with all clusters removed (i.e. the corresponding edges
+        are contracted) that are incompatible with T2.
+        
+    References
+    ----------
+    .. [1] J. Jansson, C. Shen, W.-K. Sung.
+       Improved Algorithms for Constructing Consensus Trees.
+       In: J. ACM 63, 3, Article 28 (June 2016), 24 pages.
+       doi:10.1145/2925985.
+    """
+    
+    # T2 gets copied in _preprocess, so input trees remain unchanged
+    T2, L_T2, lca, depth, start, stop, x_left, x_right = _preprocess(T1, T2)
+    
+    # size of all cluster in T1
+    size = {}
+    
+    # edges uv such that L(T1(v)) is a cluster that is not compatible with T2
+    bad_edges = []
+    
+    for u in T1.postorder():
+        
+        size[u] = sum(size[c] for c in u.children) if u.children else 1
+        
+        if not u.children:
+            continue
+        
+        a = L_T2[start[u]]
+        b = L_T2[stop[u]]
+        r_u = lca(a, b)
+        
+        leftmost_child = r_u.children[0]
+        rightmost_child = r_u.children[-1]
+        
+        d_u = x_left[a] if depth[x_left[a]] > depth[r_u] else leftmost_child
+        e_u = x_right[b] if depth[x_right[b]] > depth[r_u] else rightmost_child
+        
+        if ((d_u.parent is not r_u) or (e_u.parent is not r_u) or
+            (size[u] != stop[u] - start[u] + 1)):
+            bad_edges.append((u.parent, u))
+    
+    T = T1.copy()
+    T.contract(bad_edges)
+    
+    if return_no_of_contractions:
+        return T, len(bad_edges)
+    else:
+        return T
+
             
 if __name__ == '__main__':
     
@@ -127,7 +339,9 @@ if __name__ == '__main__':
     
     T1 = partial_trees[0]
     T2 = partial_trees[1]
-    T = merge_trees(T1, T2)
+    # T = merge_trees(T1, T2)
+    # T = loose_consensus_tree([T1, T2])
+    T = loose_consensus_tree(partial_trees)
     print(T1.to_newick())
     print(T2.to_newick())
     print(T.to_newick())
