c213728171abf51646cba630beea103f0759a43d
David Schaller <38377210+david-schaller@users.noreply.github.com>
2022-05-04 11:21:53 +0200

    create pyproject.toml and setup.cfg

--- setup.py
+++ setup.py
@@ -1,27 +1,7 @@
-import setuptools
+# -*- coding: utf-8 -*-
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name='tralda',
-    version='1.0.1',
-    author='David Schaller',
-    author_email='sdavid@bioinf.uni-leipzig.de',
-    description='A library for tree data structures and algorithms.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/david-schaller/tralda',
-    packages=setuptools.find_packages('src'),
-    package_dir={'': 'src'},
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.7',
-    install_requires=[
-        'numpy>=1.16.4',
-        'networkx>=2.2',
-   ],
-)
+if __name__ == '__main__':
+    
+    setup()
