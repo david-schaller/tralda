25b08cd39b55ebbdfd441f93ef49cc3c01a85c18
David Schaller <38377210+david-schaller@users.noreply.github.com>
2023-10-02 12:36:54 +0200

    test(bst): unit tests for binary search trees

---
+++ tests/bst_tests.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import unittest, os, random
+
+import numpy as np
+
+from tralda.datastructures.binary_tree_simple import BinarySearchTree
+from tralda.datastructures.binary_tree_avl import TreeSet, TreeDict
+
+
+__author__ = 'David Schaller'
+
+
+class TestTrees(unittest.TestCase):
+    
+    test_sequence = np.random.default_rng(seed=0).permutation(10000)
+    to_remove = [99, 7890, 0, 567, 1234, 4567, 345, 9876]
+    reference_after_removal = sorted(set(test_sequence) - set(to_remove))
+    
+    def _insertion_and_removal(self, tree_class):
+        # new instance
+        tree = tree_class()
+        
+        for x in self.test_sequence:
+            tree.add(x)
+            
+        # copy tree to make sure copying works
+        tree = tree.copy()
+        
+        for x in self.to_remove:
+            tree.remove(x)
+            
+        self.assertEqual(len(tree), len(self.reference_after_removal))
+        
+        self.assertEqual(
+            self.reference_after_removal,
+            [x for x in tree]
+        )
+    
+    
+    def test_simple_search_tree(self):
+        self._insertion_and_removal(BinarySearchTree)
+    
+    
+    def test_simple_search_tree(self):
+        self._insertion_and_removal(TreeSet)
+            
+
+if __name__ == '__main__':
+    
+    unittest.main()
