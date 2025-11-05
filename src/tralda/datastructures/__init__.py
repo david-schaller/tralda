f8f1c9d876d33545a092b7e0127a0476b30a171e
David Schaller <38377210+david-schaller@users.noreply.github.com>
2025-11-05 21:06:48 +0100

    refactor(last_common_ancestor): move LCA to separate module

--- src/tralda/datastructures/__init__.py
+++ src/tralda/datastructures/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from tralda.datastructures.tree import Tree as Tree
 from tralda.datastructures.tree import TreeNode as TreeNode
-from tralda.datastructures.tree import LCA as LCA
+from tralda.datastructures.last_common_ancestor import LCA as LCA
 from tralda.datastructures.linked import LinkedList as LinkedList
 from tralda.datastructures.linked import LinkedListNode as LinkedListNode
 from tralda.datastructures.doubly_linked import DLList as DLList
