f9687ac9be8b4053ec72294aa81875b11e27acdc
David Schaller <38377210+david-schaller@users.noreply.github.com>
2025-11-08 17:38:06 +0100

    style(linked): add type hints and update doc style

--- src/tralda/datastructures/linked.py
+++ src/tralda/datastructures/linked.py
@@ -1,12 +1,12 @@
 """Linked list.
 
-Implementation of a linked list in Python 3. The list enables accessnto single list elements in
-order to modify/delete values in constant time.
+The list enables access to single list elements in order to modify/delete values in constant time.
 """
 
 from __future__ import annotations
 
-import collections
+from collections.abc import Iterable
+from typing import Any
 
 
 class LinkedListNode:
@@ -14,11 +14,22 @@ class LinkedListNode:
 
     __slots__ = ("_value", "_next")
 
-    def __init__(self, value, next_node=None):
+    def __init__(self, value: Any, next_node: LinkedListNode | None = None) -> None:
+        """Constructor of the inked list node.
+
+        Args:
+            value: The value that the list node shall hold.
+            next_node: The successor node in the doubly-linked list.
+        """
         self._value = value
         self._next = next_node
 
-    def get(self):
+    def get(self) -> Any:
+        """Getter for the value.
+
+        Returns:
+            The value held by the list node.
+        """
         return self._value
 
 
@@ -27,48 +38,99 @@ class LinkedList:
 
     __slots__ = ("_first", "_last", "_count")
 
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
+        """Constructor of the inked list.
+
+        Args:
+            args: A single value or an iterable of values to be added to the list.
+        """
         self._first = None
         self._last = None
         self._count = 0
-
         for arg in args:
-            if isinstance(arg, collections.abc.Iterable):
+            if isinstance(arg, Iterable):
                 for item in arg:
                     self.append(item)
             else:
                 self.append(item)
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """Number of items in the list.
+
+        Returns:
+            The number of items in the list.
+        """
         return self._count
 
     def __nonzero__(self):
-        return True if self._count > 0 else False
+        """Returns whether the list is non-empty.
 
-    def __iter__(self):
+        Returns:
+            Return True if the list is non-empty.
+        """
+        return self._count > 0
+
+    def __iter__(self) -> LinkedListIterator:
+        """Returns an iterator object for the list.
+
+        Returns:
+            An iterator for this list.
+        """
         return LinkedListIterator(self)
 
     def __next__(self):
         pass
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> Any:
+        """Access value at a specified index.
+
+        Returns:
+            The value at the specified index.
+        """
         return self.node_at(index)._value
 
-    def first(self):
+    def first(self) -> Any:
+        """Return the first item in the list.
+
+        Returns:
+            The first item in the list.
+        """
         return self._first._value
 
-    def last(self):
+    def last(self) -> Any:
+        """Return the last item in the list.
+
+        Returns:
+            The last item in the list.
+        """
         return self._last._value
 
-    def first_node(self):
+    def first_node(self) -> LinkedListNode:
+        """Return the first list node.
+
+        Returns:
+            The first node of the list.
+        """
         return self._first
 
-    def node_at(self, index):
+    def node_at(self, index: int) -> LinkedListNode:
+        """Return the list node at the specified index (in O(n) time).
+
+        The index can also be negative in which case it is counted from the end (as for built-in
+        Python lists).
+
+        Returns:
+            The list node at the specified index.
+
+        Raises:
+            TypeError: If 'index' is not an integer.
+            IndexError: If the index is out of bounds.
+        """
         if not isinstance(index, int):
             raise TypeError("index must be of type 'int'")
 
         if index < (-self._count) or index >= self._count:
-            raise IndexError("index {} is out of bounds".format(index))
+            raise IndexError(f"index {index} is out of bounds")
 
         if index < 0:
             index += self._count
@@ -80,26 +142,51 @@ class LinkedList:
 
         return node
 
-    def append(self, value):
+    def append(self, value: Any) -> LinkedListNode:
+        """Append an item to the list.
+
+        Args:
+            value: The value to be inserted at the end of the list.
+
+        Returns:
+            The new end node of the list.
+        """
         new_end = LinkedListNode(value)
         if self._last:
             self._last._next = new_end
+
         self._last = new_end
         if not self._first:
             self._first = new_end
+
         self._count += 1
+
         return new_end
 
-    def extend(self, iterable):
+    def extend(self, iterable: Iterable) -> None:
+        """Append multiple items to the list.
+
+        Args:
+            An iterable of values to be inserted at the end of the list.
+        """
         for value in iterable:
             self.append(value)
 
-    def concatenate(self, other):
+    def concatenate(self, other: LinkedList) -> LinkedList:
         """Merges two linked lists.
 
-        The instance for which this function is called is extended, the other
-        list should no longer be used."""
+        The instance for which this function is called is extended, the other list should no longer
+        be used.
 
+        Args:
+            other: The liked list to concatenate with this list.
+
+        Returns:
+            This LikedList instance.
+
+        Raises:
+            TypeError: If 'other' is not a LinkedList instance.
+        """
         if not isinstance(other, LinkedList):
             raise TypeError("must be of type 'LinkedList'")
 
@@ -115,17 +202,33 @@ class LinkedList:
 
         return self
 
-    def append_left(self, value):
+    def append_left(self, value: Any) -> LinkedListNode:
+        """Append an item to left side of the list.
+
+        Args:
+            value: The value to be inserted at the beginning of the list.
+
+        Returns:
+            The new start node of the list.
+        """
         new_start = LinkedListNode(value, next_node=self._first)
         self._first = new_start
         if not self._last:
             self._last = new_start
+
         self._count += 1
+
         return new_start
 
-    def remove(self, value):
-        """Remove an item by value in O(n)."""
+    def remove(self, value: Any) -> Any:
+        """Remove an item by value (in O(n) time).
 
+        Args:
+            value: The value to be removed.
+
+        Raises:
+            KeyError: If the value was not found in the list.
+        """
         node = self._first
         prev_node = None
 
@@ -145,14 +248,20 @@ class LinkedList:
             prev_node = node
             node = node._next
 
-        raise KeyError("value {} is not in the linked list".format(value))
+        raise KeyError(f"value {value} is not in the linked list")
 
-    def insert_right_of(self, node, value):
-        """Insert a new item right of a node of the list in O(1)."""
+    def insert_right_of(self, node: LinkedListNode, value: Any) -> LinkedListNode:
+        """Insert a new item right of a node of the list in O(1).
 
+        Args:
+            node: The list node next to which the item shall be inserted.
+            value: The value to be inserted.
+
+        Returns:
+            The list node for the inserted value.
+        """
         if node is self._last:
             new_node = self.append(value)
-
         else:
             new_node = LinkedListNode(value, next_node=node._next)
             node._next = new_node
@@ -160,9 +269,12 @@ class LinkedList:
 
         return new_node
 
-    def truncate(self, index):
-        """Truncate all nodes starting from the specified index."""
+    def truncate(self, index: int) -> None:
+        """Truncate all nodes starting from the specified index in O(n) time.
 
+        Args:
+            index: The index from which on the list is truncated.
+        """
         if index <= 0:
             self.clear()
         else:
@@ -172,9 +284,12 @@ class LinkedList:
                 new_end._next = None
             self._count = index
 
-    def truncate_left(self, n):
-        """Truncate n nodes on the left side."""
+    def truncate_left(self, n: int) -> None:
+        """Truncate n nodes on the left side.
 
+        Args:
+            n: The number of items to be truncated.
+        """
         if n >= self._count:
             self.clear()
         else:
@@ -182,31 +297,38 @@ class LinkedList:
             self._first = new_start
             self._count -= n
 
-    # def popright(self):
-    #     """Removes the last node of the list and returns its value."""
+    def popleft(self) -> Any:
+        """Removes the first item of the list and returns its value.
 
-    #     pass
+        Returns:
+            The former first item of the list which has been removed.
 
-    def popleft(self):
-        """Removes the first node of the list and returns its value."""
-
+        Raises:
+            IndexError: If the list is empty.
+        """
         if self._first:
             value = self._first._value
             self._first = self._first._next
             self._first._next = None
             self._count -= 1
             return value
-        else:
-            raise IndexError("pop from empty linked list")
 
-    def clear(self):
+        raise IndexError("cannot pop from empty linked list")
+
+    def clear(self) -> None:
+        """Clear the list."""
         self._first = None
         self._last = None
         self._count = 0
 
     def _count_actual(self):
-        """Counts the actual number of nodes."""
+        """Counts the actual number of elements (for debugging purposes).
 
+        Runs in O(n) time.
+
+        Returns:
+            The counted number of items in the list.
+        """
         current = self._first
         counter = 0
 
@@ -220,14 +342,28 @@ class LinkedList:
 class LinkedListIterator:
     """Iterator class for linked list."""
 
-    def __init__(self, llist):
+    def __init__(self, llist: LinkedList) -> None:
+        """Constructor of LinkedListIterator.
+
+        Args:
+            llist: The linked list.
+        """
         self.llist = llist
         self._current = llist._first
 
-    def __iter__(self):
+    def __iter__(self) -> LinkedListIterator:
+        """An iterator for the doubly-linked list."""
         return self
 
-    def __next__(self):
+    def __next__(self) -> Any:
+        """Return the next item in the list.
+
+        Raises:
+            StopIteration: If there are no more items.
+
+        Returns:
+            The next item in the iteration.
+        """
         if self._current:
             x = self._current
             self._current = self._current._next
